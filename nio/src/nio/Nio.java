53250ae8bbd2bd6856122e96ca999dca2b3d937d
Karan Vishwakarma <40296023+kdv2807@users.noreply.github.com>
2018-07-21 12:02:53 +0530

    Add files via upload

---
+++ nio/src/nio/Nio.java
@@ -0,0 +1,24 @@
+package nio;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class Nio {
+   public static void main(String[] args) throws IOException {
+        //Path one=Paths.get("E:\\karan_java");                              //to create new folder
+        //Files.createDirectory(one);
+//            Path src=Paths.get("E:\\karan\\Demo.txt");                     //to move the file when file already exist
+//            Path dest=Paths.get("E:\\karan_java\\Demo.txt");
+//            Files.move(src, dest, StandardCopyOption.REPLACE_EXISTING);
+              Path src=Paths.get("E:\\karan\\Demo.txt");                  //to copy the file with attributes
+              Path dest=Paths.get("E:\\karan_java\\Demo.txt");
+              Files.move(src, dest);
+//            Path src=Paths.get("E:\\karan\\Demo.txt");                  //to copy the file when file exist in folder
+//            Path dest=Paths.get("E:\\karan_java\\Demo.txt");
+//            Files.copy(src, dest,StandardCopyOption.REPLACE_EXISTING);
+   }
+    
+}
